36e2cfc28651dd15a8ee8f6ee7af15ab80c228d6
Tuck <Tunchikrazy@icloud.com>
2025-10-01 13:00:35 -0500

    start over

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,35 @@ import Dashboard from './components/Dashboard/Dashboard';
 
 import { UserContext } from './contexts/UserContext';
 
-const App = () => {
-  const { user } = useContext(UserContext);
-  
+const App=()=> {
   return (
     <>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
-      </Routes>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="p-6 max-w-5xl mx-auto">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/user/*" element={
+            <ProtectedRoute role="user">
+              <UserDashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/pro/*" element={
+            <ProtectedRoute role="professional">
+              <ProDashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </main>
+    </div>
     </>
   );
-};
+}
 
 export default App;
